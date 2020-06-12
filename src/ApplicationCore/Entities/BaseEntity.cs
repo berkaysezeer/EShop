379565bedb88d2417b8893d48f141ec4edb6ba20
berkaysezeer <berkaysezeer@hotmail.com>
2020-06-12 18:40:19 +0300

    Entities & ShopContext

---
+++ src/ApplicationCore/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ApplicationCore.Entities
+{
+    public abstract class BaseEntity //abs olunca new yapilamaz
+    {
+        public virtual int Id { get; set; }
+    }
+}
